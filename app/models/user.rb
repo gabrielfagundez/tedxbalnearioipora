a8600921d74041759473e6a0a5bed8519c08dc41
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2016-04-04 23:14:43 -0300

    Add proper style and data to profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   def self.load_seeds
-    User.create(email: 'gabriel.fagundez@moove-it.com', password: '123mooveit', password_confirmation: '123mooveit')
+    User.create(email: 'gabriel.fagundez@moove-it.com', first_name: 'Gabriel', last_name: 'Fagúndez de los Reyes', password: '123mooveit', password_confirmation: '123mooveit')
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
   end
 
 end

6282bb9aa5fdedba7b1849d3fc2d3388ee06acf8
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2016-04-17 13:39:39 -0300

    Improved login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,16 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
+  layout :layout_by_resource
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      "login"
+    else
+      "application"
+    end
+  end
+
 end

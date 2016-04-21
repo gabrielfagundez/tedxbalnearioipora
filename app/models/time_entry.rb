f5b5a6733ad2f21b372355795e10ddc62d80ba97
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2016-04-21 15:54:14 -0300

    Displays only 7 days in the home

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -6,12 +6,18 @@ class TimeEntry < ActiveRecord::Base
   belongs_to :user
   belongs_to :time_category
 
+  before_save :set_duration
+
   def self.closed
     where.not(ended_at: nil)
   end
 
   def self.by_started_at
     order("started_at DESC")
+  end
+
+  def self.last_7_days
+    where("started_at > ?", Time.now - 7.days)
   end
 
   def pretty_data
@@ -29,6 +35,16 @@ class TimeEntry < ActiveRecord::Base
 
   def format_date
     DAYS[self.started_at.strftime("%w").to_i] + self.started_at.strftime(", %d %b")
+  end
+
+  private
+
+  def set_duration
+    if self.started_at.present? && self.ended_at.present?
+      self.duration = (self.ended_at - self.started_at).round
+    else
+      self.duration = 0
+    end
   end
 
 end

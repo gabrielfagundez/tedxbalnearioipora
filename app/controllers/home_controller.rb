8912a19a1c858a70c78df41deba98fe8886b5508
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2016-04-25 21:49:30 -0300

    Add ability to see upcoming events

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
     projects = Project.where(client_id: current_user.clients.collect(&:id))
     @upcoming_project_events = ProjectEvent.order('expected_date').where(project_id: projects.collect(&:id)).where("expected_date > ?", Time.now.utc).limit(3)
 
-    @upcoming_user_events = UserEvent.order('start_date').all
+    @upcoming_user_events = UserEvent.where(user_id: current_account.users.collect(&:id)).order('start_date').all
   end
 
 end
--- app/controllers/upcoming_events_controller.rb
+++ app/controllers/upcoming_events_controller.rb
@@ -4,7 +4,11 @@ class UpcomingEventsController < ApplicationController
     @users = current_account.users
     @projects = Project.where(client_id: current_user.clients.collect(&:id))
     @clients = current_account.clients
-    @upcoming_user_events = UserEvent.order('start_date').all
+
+    @upcoming_user_events = UserEvent.where(user_id: current_account.users.collect(&:id)).order('start_date').all
+
+    projects = Project.where(client_id: current_user.clients.collect(&:id))
+    @upcoming_project_events = ProjectEvent.order('expected_date').where(project_id: projects.collect(&:id)).where("expected_date > ?", Time.now.utc).limit(3)
   end
 
   def create

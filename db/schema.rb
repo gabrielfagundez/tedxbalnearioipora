6417ba17206d098e6f55d735262aa651a6e66c58
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2016-04-27 20:32:19 -0300

    Add schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160423164753) do
+ActiveRecord::Schema.define(version: 20160427231616) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
@@ -165,6 +165,7 @@ ActiveRecord::Schema.define(version: 20160423164753) do
     t.decimal  "uxui"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "user_id"
   end
 
 end

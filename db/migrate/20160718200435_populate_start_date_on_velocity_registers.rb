25038852d496286b28c736878dd0222443c5f284
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2016-07-18 20:33:01 -0300

    Hardcode for the release

--- db/migrate/20160718200435_populate_start_date_on_velocity_registers.rb
+++ db/migrate/20160718200435_populate_start_date_on_velocity_registers.rb
@@ -2,7 +2,7 @@ class PopulateStartDateOnVelocityRegisters < ActiveRecord::Migration
   def up
     VelocityRegister.all.each do |vr|
       vr.start_date = vr.created_at
-      vr.end_date = vr.created_at + vr.project.velocity_frequency_in_days.days
+      vr.end_date = vr.created_at + 7.days
       vr.save!
     end
   end
